9717385c6fd0ed49c00d2c69683c47a67b6f6c85
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2021-12-12 23:39:10 +0100

    Start Day2

--- marten/Day-02/part2.py
+++ marten/Day-02/part2.py
@@ -1,18 +1,18 @@
 from pathlib import Path
 import re
 
-def steer(path_to_input: Path) -> int:
+def steer_corrected(path_to_input: Path) -> int:
     """Steers the submarine"""
     with open(path_to_input, 'r') as f:
         input_lst = [re.sub('\n', '', i) for i in (f.readlines())]
 
     horizontal = sum([int(i[~0]) for i in input_lst if "forward" in i])
-    depth = sum([int(i[~0]) for i in input_lst if "down" in i])-sum([int(i[~0]) for i in input_lst if "up" in i])
+    aim = sum([int(i[~0]) for i in input_lst if "down" in i])-sum([int(i[~0]) for i in input_lst if "up" in i])
 
     return depth*horizontal
 
 
 if __name__ == "__main__":
-    print(steer("test.txt"))
-    print(steer("input.txt"))
+    print(steer_corrected("test.txt"))
+    print(steer_corrected("input.txt"))
 
